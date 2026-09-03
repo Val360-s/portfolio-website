5202e0ae1a13de13ebcfc71699f77938086a84f0
Val360-s <v.bocharnikova@students.clark.edu>
2026-09-02 20:06:11 -0700

    name

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,9 @@ module.exports = {
   siteMetadata: {
     title: 'Valentina Bocharnikova',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Valentina Bocharnikova is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://portfolio-website-lemon-six-57.vercel.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Valentina Bocharnikova',
+        short_name: 'Valentina Bocharnikova',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
